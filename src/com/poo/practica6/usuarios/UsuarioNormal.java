79948bb5cc5bfda46e70c3bba5d3b5e64c7ee23c
colpim2 <pamela.castle@hotmail.com>
2020-11-14 00:18:39 -0600

    Pasillo Papeleria y Electronicos ready

--- src/com/poo/practica6/usuarios/UsuarioNormal.java
+++ src/com/poo/practica6/usuarios/UsuarioNormal.java
@@ -1,5 +1,7 @@
 package com.poo.practica6.usuarios;
 import com.poo.practica6.supermercado.departamentos.electronica.*;
+import com.poo.practica6.supermercado.departamentos.papeleria.*;
+import com.poo.practica6.supermercado.departamentos.ropa.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +17,7 @@ public class UsuarioNormal {
     /** Método Constructor */
     public UsuarioNormal() {
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Nuevo Usuario\nIngrese Nombre Completo: ");
+        System.out.print("Bienvenido\nIngrese su nombre: ");
         this.nombre = teclado.nextLine();
 
         this.carrito = new ArrayList<String>();
@@ -29,14 +31,27 @@ public class UsuarioNormal {
         //precios.add(get.Precio());
         //System.out.println(get.Producto()+"Agregado al carrito");
         System.out.println("Pasillo: Papeleria");
-        //carrito.add(get.Producto());
-        //precios.add(get.Precio());
-        //System.out.println(get.Producto()+"Agregado al carrito");
+        Cuadernos cuadernos = new Cuadernos();
+        precios.add(cuadernos.getPrecio());
+        carrito.add("Cuadernos");
+        System.out.println("  Cuadernos "+cuadernos.getMarca()+" $"+cuadernos.getPrecio()+" agregado al carrito");
+        Hojas hojas = new Hojas();
+        precios.add(hojas.getPrecio());
+        carrito.add("Hojas");
+        System.out.println("  Hojas "+hojas.getMarca()+" $"+hojas.getPrecio()+" agregado al carrito");
         System.out.println("Pasillo: Electronicos");
         Lavadora lavadora = new Lavadora();
         precios.add(lavadora.getPrecio());
         carrito.add("Lavadora");
         System.out.println("  Lavadora "+lavadora.getMarca()+" $"+lavadora.getPrecio()+" agregado al carrito");
+        Refrigerador refrigerador = new Refrigerador();
+        precios.add(refrigerador.getPrecio());
+        carrito.add("Refrigerador");
+        System.out.println("  Refrigerador "+refrigerador.getMarca()+" $"+refrigerador.getPrecio()+" agregado al carrito");
+        IPhone iPhone = new IPhone("Gris");
+        precios.add(iPhone.getPrecio());
+        carrito.add("IPhone");
+        System.out.println("  Celular "+iPhone.getMarca()+" $"+iPhone.getPrecio()+" agregado al carrito");
         System.out.println("Pasillo: Ropa");
         //carrito.add(get.Producto());
         //precios.add(get.Precio());
