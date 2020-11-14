5e70457607b3ea13e6ecfcfc3ad49b872904dd83
LlamameAsh <JossCuriel117@gmail.com>
2020-11-13 23:56:34 -0600

    Manzanas listas

--- src/com/poo/practica6/supermercado/departamentos/viveres/Frutas.java
+++ src/com/poo/practica6/supermercado/departamentos/viveres/Frutas.java
@@ -1,4 +1,15 @@
 package com.poo.practica6.supermercado.departamentos.viveres;
 
-public class Frutas {
+public abstract class Frutas extends Viveres{
+    private float pesoKG;
+
+    public void setPeso(float pesoKG){
+        this.pesoKG = pesoKG;
+    }
+
+    public float getPeso(){
+        return pesoKG;
+    }
 }
+
+
---
+++ src/com/poo/practica6/supermercado/departamentos/viveres/Manzanas.java
@@ -0,0 +1,19 @@
+package com.poo.practica6.supermercado.departamentos.viveres;
+
+public class Manzanas extends Frutas{
+    private String color;
+
+    public Manzanas(){
+        setPrecio((float) 29.90);
+        setCodigoIdentificacion("MG1KG");
+        setMarca("GOLDEN");
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
+}
