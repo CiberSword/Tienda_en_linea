203ae67c24a0c2c31a722f214011ab35efe242d6
LlamameAsh <JossCuriel117@gmail.com>
2020-11-13 23:39:31 -0600

    Ya tenemos cuadernos Scribe :33

--- src/com/poo/practica6/supermercado/departamentos/PruebaProductos.java
+++ src/com/poo/practica6/supermercado/departamentos/PruebaProductos.java
@@ -2,6 +2,7 @@ package com.poo.practica6.supermercado.departamentos;
 
 import com.poo.practica6.supermercado.departamentos.*;
 import com.poo.practica6.supermercado.departamentos.electronica.*;
+import com.poo.practica6.supermercado.departamentos.papeleria.*;
 
 public class PruebaProductos{
 
@@ -10,15 +11,17 @@ public class PruebaProductos{
         Productos cel2 = new IPhone("Rosa");
         Productos cel3 = new Huawei("Blanco");
 
-        System.out.println("Celular" + cel1.getMarca());
-        System.out.println("Celular" + cel2.getMarca());
-        System.out.println("Celular" + cel3.getMarca());
+        System.out.println("Celular " + cel1.getMarca());
+        System.out.println("Celular " + cel2.getMarca());
+        System.out.println("Celular " + cel3.getMarca());
 
         Productos elec1 = new Lavadora();
         Productos elec2 = new Refrigerador();
 
-        System.out.println("Lavadora" + elec1.getMarca());
-        System.out.println("Refrigerador" +  elec2.getMarca());
+        System.out.println("Lavadora " + elec1.getMarca());
+        System.out.println("Refrigerador " +  elec2.getMarca());
+
+        Productos pap1 = new Cuadernos();
     }
 }
 
--- src/com/poo/practica6/supermercado/departamentos/papeleria/Cuadernos.java
+++ src/com/poo/practica6/supermercado/departamentos/papeleria/Cuadernos.java
@@ -1,4 +1,32 @@
 package com.poo.practica6.supermercado.departamentos.papeleria;
 
-public class Cuadernos {
+import com.poo.practica6.supermercado.departamentos.Productos;
+
+public class Cuadernos extends Productos{
+    private String tipoHoja;
+    private int numeroHojas;
+
+    public Cuadernos(){
+        setPrecio((float) 37.40);
+        setCodigoIdentificacion("CS100H");
+        setMarca("SCRIBE");
+        setTipoHoja("Cuadro chico");
+        setNumeroHojas(100);
+    }
+
+    public void setTipoHoja(String tipoHoja){
+        this.tipoHoja = tipoHoja;
+    }
+
+    public String getTipoHoja(){
+        return tipoHoja;
+    }
+
+    public void setNumeroHojas(int numeroHojas){
+        this.numeroHojas = numeroHojas;
+    }
+
+    public int getNumeroHojas(){
+        return numeroHojas;
+    }
 }
